334ab168831cdfcce84c53a2ecd0d93186897976
Jocatins <jocakhamen@gmail.com>
2020-10-15 09:24:52 +0100

    sync grids

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {
+  GridModule,
+  PagerModule,
+  PageService,
+  SortService,
+  GroupService,
+} from '@syncfusion/ej2-angular-grids';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, GridModule, PagerModule],
+  providers: [PageService, SortService, GroupService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
