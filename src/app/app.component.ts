b2c1494f953d3a2df0961684f8840c9fe5da3076
Jocatins <jocakhamen@gmail.com>
2020-10-15 17:11:23 +0100

    ejs grid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,40 +1,68 @@
 import { Component } from '@angular/core';
-import { PageSettingsModel } from '@syncfusion/ej2-angular-grids';
+import {
+  EditSettingsModel,
+  IEditCell,
+  ToolbarItems,
+  SaveEventArgs,
+} from '@syncfusion/ej2-angular-grids';
+import { Internationalization } from '@syncfusion/ej2-base';
 
 @Component({
   selector: 'app-root',
   template: `<ejs-grid
     [dataSource]="data"
-    [allowPaging]="true"
-    [pageSettings]="pageSettings"
-    [allowSorting]="true"
-    [allowGrouping]="true"
+    [toolbar]="toolbarOption"
+    [editSettings]="editOption"
+    (actionBegin)="actionBegin($event)"
   >
     <e-columns>
       <e-column
         field="OrderID"
         headerText="Order ID"
         textAlign="Right"
-        width="90"
+        editType="numericedit"
+        width="50"
       ></e-column>
       <e-column
         field="CustomerID"
         headerText="Customer ID"
-        width="120"
+        editType="dropdownedit"
+        [edit]="deParams"
+        width="50"
       ></e-column>
+      <e-column
+        field="EmployeeID"
+        headerText="Employee ID"
+        editType="dropdownedit"
+        [edit]="deParams"
+        width="50"
+        format="P"
+      ></e-column>
+      <e-column
+        field="Validity"
+        headerText="Validity Period"
+        width="50"
+        [valueAccessor]="valueAccess"
+      >
+        <ng-template #editTemplate>
+          <ejs-daterangepicker [(value)]="orderData.Validity" format="yMd">
+          </ejs-daterangepicker>
+        </ng-template>
+      </e-column>
       <e-column
         field="Freight"
         headerText="Freight"
         textAlign="Right"
         format="C2"
-        width="90"
+        editType="numericedit"
+        width="50"
       ></e-column>
       <e-column
         field="OrderDate"
         headerText="Order Date"
         textAlign="Right"
         format="yMd"
-        width="120"
+        width="50"
       ></e-column>
     </e-columns>
   </ejs-grid>`,
@@ -43,7 +71,48 @@ import { PageSettingsModel } from '@syncfusion/ej2-angular-grids';
 })
 export class AppComponent {
   title = 'grid';
-  public pageSettings: PageSettingsModel = { pageSize: 5 };
+  public editOption: EditSettingsModel = {
+    allowAdding: true,
+    allowEditing: true,
+    allowDeleting: true,
+    mode: 'Dialog',
+  };
+  public toolbarOption: ToolbarItems[] = [
+    'Add',
+    'Edit',
+    'Delete',
+    'Update',
+    'Cancel',
+  ];
+  public deParams: IEditCell = { params: { value: '' } };
+  public orderData: object;
+  public val: any;
+  public Intl: Internationalization = new Internationalization();
+
+  actionBegin(args: SaveEventArgs) {
+    if (args.requestType === 'beginEdit' || args.requestType === 'add') {
+      this.orderData = Object.assign({}, args.rowData);
+    }
+    if (args.requestType === 'save') {
+      const Validity = 'Validity';
+      args.data[Validity] = this.orderData[Validity];
+    }
+  }
+  public dateformatter = (value: any) => {
+    let dFormatter: Function = this.Intl.getDateFormat({
+      skeleton: 'y',
+      type: 'date',
+    });
+    return dFormatter(new Date(value));
+  };
+  public valueAccess = (field: string, value: object, column: object) => {
+    this.val =
+      this.dateformatter(new Date(value[field][0])) +
+      ' -' +
+      this.dateformatter(new Date(value[field][1]));
+    return this.val;
+  };
+
   public data: object[] = [
     {
       OrderID: 10248,
@@ -58,6 +127,7 @@ export class AppComponent {
       ShipCountry: 'France',
       Freight: 32.38,
       Verified: !0,
+      Validity: [new Date(2020, 2, 2), new Date(2023, 7, 10)],
     },
     {
       OrderID: 10249,
@@ -72,6 +142,7 @@ export class AppComponent {
       ShipCountry: 'Germany',
       Freight: 11.61,
       Verified: !1,
+      Validity: [new Date(2020, 2, 2), new Date(2023, 7, 10)],
     },
     {
       OrderID: 10250,
@@ -86,174 +157,7 @@ export class AppComponent {
       ShipCountry: 'Brazil',
       Freight: 65.83,
       Verified: !0,
-    },
-    {
-      OrderID: 10251,
-      CustomerID: 'VICTE',
-      EmployeeID: 3,
-      OrderDate: new Date(8367642e5),
-      ShipName: 'Victuailles en stock',
-      ShipCity: 'Lyon',
-      ShipAddress: '2, rue du Commerce',
-      ShipRegion: 'CJ',
-      ShipPostalCode: '69004',
-      ShipCountry: 'France',
-      Freight: 41.34,
-      Verified: !0,
-    },
-    {
-      OrderID: 10252,
-      CustomerID: 'SUPRD',
-      EmployeeID: 4,
-      OrderDate: new Date(8368506e5),
-      ShipName: 'Suprêmes délices',
-      ShipCity: 'Charleroi',
-      ShipAddress: 'Boulevard Tirou, 255',
-      ShipRegion: 'CJ',
-      ShipPostalCode: 'B-6000',
-      ShipCountry: 'Belgium',
-      Freight: 51.3,
-      Verified: !0,
-    },
-    {
-      OrderID: 10253,
-      CustomerID: 'HANAR',
-      EmployeeID: 3,
-      OrderDate: new Date(836937e6),
-      ShipName: 'Hanari Carnes',
-      ShipCity: 'Rio de Janeiro',
-      ShipAddress: 'Rua do Paço, 67',
-      ShipRegion: 'RJ',
-      ShipPostalCode: '05454-876',
-      ShipCountry: 'Brazil',
-      Freight: 58.17,
-      Verified: !0,
-    },
-    {
-      OrderID: 10254,
-      CustomerID: 'CHOPS',
-      EmployeeID: 5,
-      OrderDate: new Date(8370234e5),
-      ShipName: 'Chop-suey Chinese',
-      ShipCity: 'Bern',
-      ShipAddress: 'Hauptstr. 31',
-      ShipRegion: 'CJ',
-      ShipPostalCode: '3012',
-      ShipCountry: 'Switzerland',
-      Freight: 22.98,
-      Verified: !1,
-    },
-    {
-      OrderID: 10255,
-      CustomerID: 'RICSU',
-      EmployeeID: 9,
-      OrderDate: new Date(8371098e5),
-      ShipName: 'Richter Supermarkt',
-      ShipCity: 'Genève',
-      ShipAddress: 'Starenweg 5',
-      ShipRegion: 'CJ',
-      ShipPostalCode: '1204',
-      ShipCountry: 'Switzerland',
-      Freight: 148.33,
-      Verified: !0,
-    },
-    {
-      OrderID: 10256,
-      CustomerID: 'WELLI',
-      EmployeeID: 3,
-      OrderDate: new Date(837369e6),
-      ShipName: 'Wellington Importadora',
-      ShipCity: 'Resende',
-      ShipAddress: 'Rua do Mercado, 12',
-      ShipRegion: 'SP',
-      ShipPostalCode: '08737-363',
-      ShipCountry: 'Brazil',
-      Freight: 13.97,
-      Verified: !1,
-    },
-    {
-      OrderID: 10257,
-      CustomerID: 'HILAA',
-      EmployeeID: 4,
-      OrderDate: new Date(8374554e5),
-      ShipName: 'HILARION-Abastos',
-      ShipCity: 'San Cristóbal',
-      ShipAddress: 'Carrera 22 con Ave. Carlos Soublette #8-35',
-      ShipRegion: 'Táchira',
-      ShipPostalCode: '5022',
-      ShipCountry: 'Venezuela',
-      Freight: 81.91,
-      Verified: !0,
-    },
-    {
-      OrderID: 10258,
-      CustomerID: 'ERNSH',
-      EmployeeID: 1,
-      OrderDate: new Date(8375418e5),
-      ShipName: 'Ernst Handel',
-      ShipCity: 'Graz',
-      ShipAddress: 'Kirchgasse 6',
-      ShipRegion: 'CJ',
-      ShipPostalCode: '8010',
-      ShipCountry: 'Austria',
-      Freight: 140.51,
-      Verified: !0,
-    },
-    {
-      OrderID: 10259,
-      CustomerID: 'CENTC',
-      EmployeeID: 4,
-      OrderDate: new Date(8376282e5),
-      ShipName: 'Centro comercial Moctezuma',
-      ShipCity: 'México D.F.',
-      ShipAddress: 'Sierras de Granada 9993',
-      ShipRegion: 'CJ',
-      ShipPostalCode: '05022',
-      ShipCountry: 'Mexico',
-      Freight: 3.25,
-      Verified: !1,
-    },
-    {
-      OrderID: 10260,
-      CustomerID: 'OTTIK',
-      EmployeeID: 4,
-      OrderDate: new Date(8377146e5),
-      ShipName: 'Ottilies Käseladen',
-      ShipCity: 'Köln',
-      ShipAddress: 'Mehrheimerstr. 369',
-      ShipRegion: 'CJ',
-      ShipPostalCode: '50739',
-      ShipCountry: 'Germany',
-      Freight: 55.09,
-      Verified: !0,
-    },
-    {
-      OrderID: 10261,
-      CustomerID: 'QUEDE',
-      EmployeeID: 4,
-      OrderDate: new Date(8377146e5),
-      ShipName: 'Que Delícia',
-      ShipCity: 'Rio de Janeiro',
-      ShipAddress: 'Rua da Panificadora, 12',
-      ShipRegion: 'RJ',
-      ShipPostalCode: '02389-673',
-      ShipCountry: 'Brazil',
-      Freight: 3.05,
-      Verified: !1,
-    },
-    {
-      OrderID: 10262,
-      CustomerID: 'RATTC',
-      EmployeeID: 8,
-      OrderDate: new Date(8379738e5),
-      ShipName: 'Rattlesnake Canyon Grocery',
-      ShipCity: 'Albuquerque',
-      ShipAddress: '2817 Milton Dr.',
-      ShipRegion: 'NM',
-      ShipPostalCode: '87110',
-      ShipCountry: 'USA',
-      Freight: 48.29,
-      Verified: !0,
+      Validity: [new Date(2020, 2, 2), new Date(2023, 7, 10)],
     },
   ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,15 @@ import { AppComponent } from './app.component';
 import {
   GridModule,
   PagerModule,
-  PageService,
-  SortService,
-  GroupService,
+  EditService,
+  ToolbarService,
 } from '@syncfusion/ej2-angular-grids';
+import { DateRangePickerModule } from '@syncfusion/ej2-angular-calendars';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, GridModule, PagerModule],
-  providers: [PageService, SortService, GroupService],
+  imports: [BrowserModule, GridModule, PagerModule, DateRangePickerModule],
+  providers: [EditService, ToolbarService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
